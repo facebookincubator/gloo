206c17c845e61d2f02e4871ba9c6730e1b3802ef
Pieter Noordhuis <pietern@fb.com>
2017-09-27 10:22:13 -0700

    Update eigen CMake module to match others

--- cmake/Modules/Findeigen.cmake
+++ cmake/Modules/Findeigen.cmake
@@ -2,20 +2,20 @@
 #
 # The following variables are optionally searched for defaults
 #  EIGEN_ROOT_DIR: Base directory where all eigen components are found
+#  EIGEN_INCLUDE_DIR: Directory where eigen headers are found
 #
 # The following are set after configuration is done:
 #  EIGEN_FOUND
-#  eigen_INCLUDE_DIR
+#  EIGEN_INCLUDE_DIRS
 
-find_path(eigen_INCLUDE_DIR
+find_path(EIGEN_INCLUDE_DIRS
   NAMES Eigen/Core
-  HINTS ${EIGEN_ROOT_DIR} ${EIGEN_ROOT_DIR}/include
+  HINTS
+  ${EIGEN_INCLUDE_DIR}
+  ${EIGEN_ROOT_DIR}
+  ${EIGEN_ROOT_DIR}/include
   PATH_SUFFIXES eigen3)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(eigen DEFAULT_MSG eigen_INCLUDE_DIR)
-
-if(EIGEN_FOUND)
-  message(STATUS "Found eigen (include: ${eigen_INCLUDE_DIR})")
-  mark_as_advanced(eigen_INCLUDE_DIR eigen_LIBRARIES)
-endif()
+find_package_handle_standard_args(eigen DEFAULT_MSG EIGEN_INCLUDE_DIRS)
+mark_as_advanced(EIGEN_INCLUDE_DIRS)
