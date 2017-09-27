6eca76de642ca145a4583d93898cbb5a3b60e4a6
Pieter Noordhuis <pietern@fb.com>
2017-09-27 09:50:49 -0700

    Update hiredis CMake module to match others

--- cmake/Modules/Findhiredis.cmake
+++ cmake/Modules/Findhiredis.cmake
@@ -2,28 +2,30 @@
 #
 # The following variables are optionally searched for defaults
 #  HIREDIS_ROOT_DIR: Base directory where all hiredis components are found
+#  HIREDIS_INCLUDE_DIR: Directory where hiredis headers are found
+#  HIREDIS_LIB_DIR: Directory where hiredis library is found
 #
 # The following are set after configuration is done:
 #  HIREDIS_FOUND
-#  hiredis_INCLUDE_DIR
-#  hiredis_LIBRARIES
+#  HIREDIS_INCLUDE_DIRS
+#  HIREDIS_LIBRARIES
 
-find_path(hiredis_INCLUDE_DIR
+find_path(HIREDIS_INCLUDE_DIRS
   NAMES hiredis.h
-  HINTS ${HIREDIS_ROOT_DIR} ${HIREDIS_ROOT_DIR}/include
+  HINTS
+  ${HIREDIS_INCLUDE_DIR}
+  ${HIREDIS_ROOT_DIR}
+  ${HIREDIS_ROOT_DIR}/include
   PATH_SUFFIXES hiredis)
 
-find_library(hiredis_LIBRARIES
+find_library(HIREDIS_LIBRARIES
   NAMES hiredis
-  HINTS ${HIREDIS_ROOT_DIR} ${HIREDIS_ROOT_DIR}/lib
+  HINTS
+  ${HIREDIS_LIB_DIR}
+  ${HIREDIS_ROOT_DIR}
+  ${HIREDIS_ROOT_DIR}/lib
   PATH_SUFFIXES hiredis)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(hiredis DEFAULT_MSG hiredis_INCLUDE_DIR hiredis_LIBRARIES)
-
-if(HIREDIS_FOUND)
-  set(include_message "include: ${hiredis_INCLUDE_DIR}")
-  set(library_message "library: ${hiredis_LIBRARIES}")
-  message(STATUS "Found hiredis (${include_message}, ${library_message})")
-  mark_as_advanced(hiredis_INCLUDE_DIR hiredis_LIBRARIES)
-endif()
+find_package_handle_standard_args(hiredis DEFAULT_MSG HIREDIS_INCLUDE_DIRS HIREDIS_LIBRARIES)
+mark_as_advanced(HIREDIS_INCLUDE_DIRS HIREDIS_LIBRARIES)
