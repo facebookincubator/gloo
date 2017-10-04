1f4124e71a66ab97da27133a0ef88309e27c7b1b
Pieter Noordhuis <pietern@fb.com>
2017-10-04 09:55:21 -0700

    Add optional handling of MPI lifecycle to MPI context
    Summary:
    Applications that are launched through MPI but only use Gloo directly
    shouldn't have to deal with MPI_Init/MPI_Finalize. By using the
    createManaged() function on the MPI context, an application can now
    defer dealing with MPI_Init and MPI_Finalize to Gloo.
    Closes https://github.com/facebookincubator/gloo/pull/94
    
    Differential Revision: D5967154
    
    Pulled By: pietern
    
    fbshipit-source-id: d483b0bc9f0e7071ba23ca20a22acf02b228a559

--- gloo/mpi/context.h
+++ gloo/mpi/context.h
@@ -17,14 +17,35 @@
 namespace gloo {
 namespace mpi {
 
+class MPIScope {
+ public:
+  MPIScope();
+  ~MPIScope();
+};
+
 class Context : public ::gloo::Context {
  public:
+  // This function acquires and holds on to a global MPI scope object.
+  // The MPI scope object calls MPI_Init upon construction and
+  // MPI_Finalize upon destruction. Use this function to create a
+  // context if you want this to be managed by Gloo.
+  static std::shared_ptr<Context> createManaged();
+
+  // This constructor clone the specified MPI common world. Use it if
+  // you are calling MPI_Init and MPI_Finalize yourself.
   explicit Context(const MPI_Comm& comm);
+
   virtual ~Context();
 
   void connectFullMesh(std::shared_ptr<transport::Device>& dev);
 
  protected:
+  // If Gloo is responsible for calling MPI_Init and MPI_Finalize,
+  // this context refers to a singleton initializer. As soon as the
+  // last MPI context is destructed, this initializer is destructed,
+  // and MPI_Finalize will be called.
+  std::shared_ptr<MPIScope> mpiScope_;
+
   MPI_Comm comm_;
 };
 
