ea110393b137dd0d31140eddbf0e8f4552709ea9
Peter Yeh <pyeh@amd.com>
2018-11-12 15:06:04 -0800

    hipify gloo init

--- amd_build/build_gloo_amd.py
+++ amd_build/build_gloo_amd.py
@@ -11,31 +11,17 @@ amd_build_dir = os.path.dirname(os.path.realpath(__file__))
 proj_dir = os.path.join(os.path.dirname(os.path.dirname(amd_build_dir)))
 
 includes = [
-    "caffe2/operators/*",
-    "caffe2/sgd/*",
-    "caffe2/image/*",
-    "caffe2/transforms/*",
-    "caffe2/video/*",
-    "caffe2/distributed/*",
-    "caffe2/queue/*",
-    "binaries/*",
-    "caffe2/**/*_test*",
-    "caffe2/core/*",
-    "caffe2/db/*",
-    "caffe2/utils/*",
+    "gloo/*"
 ]
 
 ignores = [
-    "caffe2/operators/depthwise_3x3_conv_op.cu",
-    "caffe2/operators/depthwise_3x3_conv_op_cudnn.cu",
-    "caffe2/operators/pool_op_cudnn.cu",
     '**/hip/**',
 ]
 
 file_extensions = ['.cc', '.cu', '.h', '.cuh']
 
 parser = argparse.ArgumentParser(
-    description="The Script to Hipify Caffe2")
+    description="The Script to Hipify Gloo")
 
 parser.add_argument(
     '--hip-suffix',
