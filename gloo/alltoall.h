eb473debad2cef06d9a11711ec033e696dd8927a
Hongyi Jia <jiayisuse@fb.com>
2020-07-02 18:49:27 -0700

    Add alltoall alltoallv collectives (#259)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/gloo/pull/259
    
    Add alltoall and alltoallv to Gloo
    
    Reviewed By: osalpekar
    
    Differential Revision: D22365203
    
    fbshipit-source-id: 6e6f19413f5836489412436c49ed6ff6113e37f4

---
+++ gloo/alltoall.h
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "gloo/common/logging.h"
+#include "gloo/context.h"
+#include "gloo/transport/unbound_buffer.h"
+
+namespace gloo {
+
+class AlltoallOptions {
+ public:
+  explicit AlltoallOptions(const std::shared_ptr<Context>& context)
+      : context(context), timeout(context->getTimeout()) {}
+
+  template <typename T>
+  void setInput(std::unique_ptr<transport::UnboundBuffer> buf) {
+    elementSize = sizeof(T);
+    in = std::move(buf);
+  }
+
+  template <typename T>
+  void setInput(T* ptr, size_t elements) {
+    elementSize = sizeof(T);
+    in = context->createUnboundBuffer(ptr, elements * sizeof(T));
+  }
+
+  template <typename T>
+  void setOutput(std::unique_ptr<transport::UnboundBuffer> buf) {
+    elementSize = sizeof(T);
+    out = std::move(buf);
+  }
+
+  template <typename T>
+  void setOutput(T* ptr, size_t elements) {
+    elementSize = sizeof(T);
+    out = context->createUnboundBuffer(ptr, elements * sizeof(T));
+  }
+
+  void setTag(uint32_t tag) {
+    this->tag = tag;
+  }
+
+  void setTimeout(std::chrono::milliseconds timeout) {
+    GLOO_ENFORCE(timeout.count() > 0);
+    this->timeout = timeout;
+  }
+
+ protected:
+  std::shared_ptr<Context> context;
+  std::unique_ptr<transport::UnboundBuffer> in;
+  std::unique_ptr<transport::UnboundBuffer> out;
+
+  // Number of bytes per element.
+  size_t elementSize = 0;
+
+  // Tag for this operation.
+  // Must be unique across operations executing in parallel.
+  uint32_t tag = 0;
+
+  // End-to-end timeout for this operation.
+  std::chrono::milliseconds timeout;
+
+  friend void alltoall(AlltoallOptions&);
+};
+
+void alltoall(AlltoallOptions& opts);
+
+} // namespace gloo
---
+++ gloo/test/alltoall_test.cc
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "gloo/alltoall.h"
+#include "gloo/test/base_test.h"
+
+namespace gloo {
+namespace test {
+namespace {
+
+// Test parameterization.
+using Param = std::tuple<Transport, int, size_t>;
+
+// Test fixture.
+class AlltoallTest : public BaseTest,
+                   public ::testing::WithParamInterface<Param> {};
+
+TEST_P(AlltoallTest, Default) {
+  const auto transport = std::get<0>(GetParam());
+  const auto contextSize = std::get<1>(GetParam());
+  const auto dataSize = std::get<2>(GetParam());
+
+  spawn(transport, contextSize, [&](std::shared_ptr<Context> context) {
+    std::vector<uint64_t> input(contextSize * dataSize);
+    std::vector<uint64_t> output(contextSize * dataSize);
+
+    for (int i = 0; i < contextSize; i++) {
+      for (int j = 0; j < dataSize; j++) {
+        input[i * dataSize + j] = context->rank * j + i * 127;
+      }
+    }
+
+    AlltoallOptions opts(context);
+    opts.setInput(input.data(), contextSize * dataSize);
+    opts.setOutput(output.data(), contextSize * dataSize);
+
+    alltoall(opts);
+
+    // Validate result.
+    for (int i = 0; i < contextSize; i++) {
+      for (int j = 0; j < dataSize; j++) {
+        ASSERT_EQ(output[i * dataSize + j], i * j + context->rank * 127);
+      }
+    }
+  });
+}
+
+INSTANTIATE_TEST_CASE_P(
+    AlltoallDefault,
+    AlltoallTest,
+    ::testing::Combine(
+        ::testing::ValuesIn(kTransportsForFunctionAlgorithms),
+        ::testing::Values(1, 2, 4, 7),
+        ::testing::Values(4, 100, 1000, 10000)));
+} // namespace
+} // namespace test
+} // namespace gloo
