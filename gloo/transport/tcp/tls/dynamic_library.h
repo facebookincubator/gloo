30f7ee51775f9f0a65ac025e7dad457138496df8
Pavel Belevich <pbelevich@fb.com>
2021-04-14 22:07:26 -0400

    Update on "Dynamic loading of OpenSSL"
    Differential Revision: [D27781296](https://our.internmc.facebook.com/intern/diff/D27781296)
    
    [ghstack-poisoned]

--- gloo/transport/tcp/tls/dynamic_library.h
+++ gloo/transport/tcp/tls/dynamic_library.h
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2021-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
 #include <string>
 
 struct DynamicLibrary {
