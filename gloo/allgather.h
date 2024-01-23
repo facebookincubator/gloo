c58d3fafb951075dd4ef04a8c679f6b90ed34e7b
Richard Barnes <rbarnes@meta.com>
2024-01-23 08:35:24 -0800

    Fix shadowed variable in gloo/allgather.h
    Summary:
    Our upcoming compiler upgrade will require us not to have shadowed variables. Such variables have a _high_ bug rate and reduce readability, so we would like to avoid them even if the compiler was not forcing us to do so.
    
    This codemod attempts to fix an instance of a shadowed variable. Please review with care: if it's failed the result will be a silent bug.
    
    **What's a shadowed variable?**
    
    Shadowed variables are variables in an inner scope with the same name as another variable in an outer scope. Having the same name for both variables might be semantically correct, but it can make the code confusing to read! It can also hide subtle bugs.
    
    This diff fixes such an issue by renaming the variable.
    
     - If you approve of this diff, please use the "Accept & Ship" button :-)
    
    Reviewed By: dmm-fb
    
    Differential Revision: D52959003
    
    fbshipit-source-id: 3911d93bed3547220a2335473ccee1130ba4484e

--- gloo/allgather.h
+++ gloo/allgather.h
@@ -42,12 +42,12 @@ class AllgatherOptions {
     out = context->createUnboundBuffer(ptr, elements * sizeof(T));
   }
 
-  void setTag(uint32_t tag) {
-    this->tag = tag;
+  void setTag(uint32_t tag_2) {
+    this->tag = tag_2;
   }
 
-  void setTimeout(std::chrono::milliseconds timeout) {
-    this->timeout = timeout;
+  void setTimeout(std::chrono::milliseconds timeout_2) {
+    this->timeout = timeout_2;
   }
 
  protected:
