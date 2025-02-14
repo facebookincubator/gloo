78fe917a001f14f24e8a98b5e479a88d72919c73
liqingping <liqingping@infini-ai.com>
2025-02-14 11:50:16 +0800

    build: do not build redis store

--- hack/build.sh
+++ hack/build.sh
@@ -8,7 +8,7 @@ mkdir -p build
 cd build
 cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DUSE_IBVERBS=1 -DBUILD_BENCHMARK=1 -DUSE_REDIS=1 \
+    -DUSE_IBVERBS=1 -DBUILD_BENCHMARK=1 \
     -DBUILD_SHARED_LIBS=1 \
     ../ 
 make
