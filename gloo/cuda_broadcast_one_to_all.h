95ca2af4e4c76433fac8911525d8a0142b7a5289
Richard Barnes <rbarnes@meta.com>
2025-03-18 18:29:34 -0400

    Fix deprecated use of 0/NULL in gloo/cuda_broadcast_one_to_all.h + 1
    Differential Revision: D71284808
    
    Pull Request resolved: https://github.com/facebookincubator/gloo/pull/420

--- gloo/cuda_broadcast_one_to_all.h
+++ gloo/cuda_broadcast_one_to_all.h
@@ -36,13 +36,13 @@ class CudaBroadcastOneToAll : public Algorithm {
   void init(
       typename std::enable_if<
           std::is_same<U, CudaHostWorkspace<T>>::value,
-          typename U::Pointer>::type* = NULL);
+          typename U::Pointer>::type* = nullptr);
 
   template <typename U = W>
   void init(
       typename std::enable_if<
           std::is_same<U, CudaDeviceWorkspace<T>>::value,
-          typename U::Pointer>::type* = NULL);
+          typename U::Pointer>::type* = nullptr);
 
   std::vector<CudaDevicePointer<T>> devicePtrs_;
   std::vector<CudaStream> streams_;
